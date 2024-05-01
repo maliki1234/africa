6dc7142a01ec8c480ec8a363a9dea0e76d72a090
maliki1234 <malikikowero@gmail.com>
2024-05-01 20:18:57 +0300

    thrdd

--- components/gallery/Gallery.tsx
+++ components/gallery/Gallery.tsx
@@ -8,7 +8,7 @@ export default class Gallery extends Component {
   render() {
     return (
      
-      <div className="pt-16 h-screen">
+      <div className="h-screen">
         <h3 className="text-center capitalize font-bold text-xl md:text-5xl p-2 md:p-4"> gallery</h3>
       <ParallaxScrollDemo/>
       </div>
